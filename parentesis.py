819a178d22bfec882d911521d94b4166ee05fbdd
Juan Diego Valenzuela <jd_valenzuelap@hotmail.com>
2021-08-10 08:04:25 -0700

    Funcion de parentesis() agregada

--- parentesis.py
+++ parentesis.py
@@ -1,1 +1,33 @@
-string = input('Ingresa el string: ')
+
+
+def parentesis(string):
+    stack = []
+
+    for elemento in string:
+        if elemento == '(' or elemento == '[':
+            stack.append(elemento)
+        elif elemento == ')':
+            if len(stack) == 0:
+                return False
+            elif stack.pop() != '(':
+                return False
+
+        elif elemento == ']':
+            if len(stack) == 0:
+                return False
+            elif stack.pop() != '[':
+                return False
+        else:
+            return False
+    # print(stack)
+
+    return len(stack) == 0
+
+    
+print(parentesis('[(()]')) #Falso
+print(parentesis('[(())]')) #Verdadero
+    
+
+
+
+
